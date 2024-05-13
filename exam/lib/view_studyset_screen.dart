bdfe1395f739503e6ca4e69d450cfb0f1f83d635
TruongGiaBao299 <truonggiabao203@gmail.com>
2024-05-13 13:55:10 +0700

    update UI view study set

--- exam/lib/view_studyset_screen.dart
+++ exam/lib/view_studyset_screen.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
 import 'library_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ViewStudySetScreen extends StatelessWidget {
   final StudySet studySet;
 
-  const ViewStudySetScreen({Key? key, required this.studySet}) : super(key: key);
+  const ViewStudySetScreen({Key? key, required this.studySet})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Retrieve current user's email from Firebase
+    String? userEmail = FirebaseAuth.instance.currentUser?.email;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Study Set Details'),
+        backgroundColor: Colors.transparent, // Set background to transparent
+        elevation: 0, // Remove the shadow
+        title: Center(
+          child: const Text('Study Set Details',
+              style: TextStyle(color: Colors.black)),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back,
+              color: Colors.black), // Set icon color to black
+          onPressed: () {
+            Navigator.of(context).pop(); // Go to previous page
+          },
+        ),
         actions: [
           IconButton(
-            icon: Icon(Icons.more_vert),
+            icon: Icon(Icons.more_vert,
+                color: Colors.black), // Set icon color to black
             onPressed: () {
               // Show options menu
             },
@@ -24,25 +42,29 @@ class ViewStudySetScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8.0),
             child: Text(
               studySet.title,
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
+                color: Colors.black, // Set text color to black
               ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8.0),
             child: Text(
-              'Created by: User Name - ${studySet.terms.length} terms',
-              style: TextStyle(fontSize: 16),
+              'Created by: $userEmail - ${studySet.terms.length} terms',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black, // Set text color to black
+              ),
             ),
           ),
           SizedBox(height: 20),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               children: [
                 Expanded(
@@ -50,7 +72,9 @@ class ViewStudySetScreen extends StatelessWidget {
                     onPressed: () {
                       // Navigate to flashcard screen
                     },
-                    child: Text('Flashcard'),
+                    child: Text('Flashcard',
+                        style: TextStyle(
+                            color: Colors.white)), // Set text color to white
                   ),
                 ),
                 SizedBox(width: 10),
@@ -59,14 +83,16 @@ class ViewStudySetScreen extends StatelessWidget {
                     onPressed: () {
                       // Navigate to learn screen
                     },
-                    child: Text('Learn'),
+                    child: Text('Learn',
+                        style: TextStyle(
+                            color: Colors.white)), // Set text color to white
                   ),
                 ),
               ],
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               children: [
                 Expanded(
@@ -74,7 +100,9 @@ class ViewStudySetScreen extends StatelessWidget {
                     onPressed: () {
                       // Navigate to test screen
                     },
-                    child: Text('Test'),
+                    child: Text('Test',
+                        style: TextStyle(
+                            color: Colors.white)), // Set text color to white
                   ),
                 ),
                 SizedBox(width: 10),
@@ -83,7 +111,9 @@ class ViewStudySetScreen extends StatelessWidget {
                     onPressed: () {
                       // Navigate to match screen
                     },
-                    child: Text('Match'),
+                    child: Text('Match',
+                        style: TextStyle(
+                            color: Colors.white)), // Set text color to white
                   ),
                 ),
               ],
@@ -91,14 +121,30 @@ class ViewStudySetScreen extends StatelessWidget {
           ),
           SizedBox(height: 20),
           Expanded(
-            child: ListView.builder(
-              itemCount: studySet.terms.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(studySet.terms[index]['term']!),
-                  subtitle: Text(studySet.terms[index]['definition']!),
-                );
-              },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SizedBox(
+                height: 100,
+                child: ListView.separated(
+                  itemCount: studySet.terms.length,
+                  separatorBuilder: (BuildContext context, int index) =>
+                      Divider(
+                    color: Colors.black,
+                    thickness: 1,
+                  ),
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text("Title: " + studySet.terms[index]['term']!,
+                          style: TextStyle(
+                              color: Colors.black)), // Set text color to black
+                      subtitle: Text(
+                          "Definition: " + studySet.terms[index]['definition']!,
+                          style: TextStyle(
+                              color: Colors.black)), // Set text color to black
+                    );
+                  },
+                ),
+              ),
             ),
           ),
         ],
